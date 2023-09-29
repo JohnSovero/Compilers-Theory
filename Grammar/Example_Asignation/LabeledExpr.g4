ffaefde11c96c776fa6ee6349e9f5c514081aa22
John Sovero <u202115065@upc.edu.pe>
2023-09-29 14:59:33 -0500

    Grammar Examples

---
+++ Grammar/Example_Asignation/LabeledExpr.g4
@@ -0,0 +1,31 @@
+grammar LabeledExpr;
+
+/* Syntax analizer */
+
+prog: stat+ ;
+
+stat
+	: expr NEWLINE          # printExpr
+	| ID '=' expr NEWLINE   # assign
+	| NEWLINE               # blank
+	;
+
+expr
+	: expr op=('*'|'/') expr   # MulDiv
+	| expr op=('+'|'-') expr   # AddSub
+	| INT                   # Int
+	| ID                    # Id
+	| '(' expr ')'          # Parens
+	;
+
+/* Lexical analizer */
+
+MUL: '*' ;
+DIV: '/' ;
+ADD: '+' ;
+SUB: '-' ;
+
+ID     : [a-zA-Z]+ ;
+INT    : [0-9]+ ;
+NEWLINE: '\r'? '\n' ;
+WS     : [ \t]+ -> skip ;
---
+++ Grammar/Example_Asignation/LabeledExpr/EvalVisitorImpl.java
@@ -0,0 +1,68 @@
+import java.util.HashMap;
+import java.util.Map;
+
+/* added the Impl at the end of the class to avoid it being .gitignored sorry */
+public class EvalVisitorImpl extends LabeledExprBaseVisitor<Integer> {
+	Map<String, Integer> memory = new HashMap<String, Integer>();
+
+	/* ID '=' expr NEWLINE */
+	@Override
+	public Integer visitAssign(LabeledExprParser.AssignContext ctx) {
+		String id = ctx.ID().getText();
+		int value = visit(ctx.expr());
+		memory.put(id, value);
+		return value;
+	}
+
+	/* expr NEWLINE */
+	@Override
+	public Integer visitPrintExpr(LabeledExprParser.PrintExprContext ctx) {
+		Integer value = visit(ctx.expr());
+		System.out.println(value);
+		return 0;
+	}
+
+	/* INT */
+	@Override
+	public Integer visitInt(LabeledExprParser.IntContext ctx) {
+		return Integer.valueOf(ctx.INT().getText());
+	}
+
+	/* ID */
+	@Override
+	public Integer visitId(LabeledExprParser.IdContext ctx) {
+		String id = ctx.ID().getText();
+		if (memory.containsKey(id)) return memory.get(id);
+		return 0;
+	}
+
+	/* expr op=('*'|'/') expr */
+	@Override
+	public Integer visitMulDiv(LabeledExprParser.MulDivContext ctx) {
+		int left = visit(ctx.expr(0));
+		int right = visit(ctx.expr(1));
+		if (ctx.op.getType() == LabeledExprParser.MUL) {
+			return left * right;
+		} else {
+			return left / right;
+		}
+	}
+
+	/* expr op=('+'|'-') expr */
+	@Override
+	public Integer visitAddSub(LabeledExprParser.AddSubContext ctx) {
+		int left = visit(ctx.expr(0));
+		int right = visit(ctx.expr(1));
+		if (ctx.op.getType() == LabeledExprParser.ADD) {
+			return left + right;
+		} else {
+			return left - right;
+		}
+	}
+
+	/* '(' expr ')' */
+	@Override
+	public Integer visitParens(LabeledExprParser.ParensContext ctx) {
+		return visit(ctx.expr());
+	}
+}
